2d56a760133127134617b01c0d5585983022f6df
Celiae <ceelia0g@gmail.com>
2022-05-15 17:54:14 +0800

    Language set to zh_CN(China)

--- components/avatar.tsx
+++ components/avatar.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 type Props = {
   name: string;
   picture: string;
@@ -6,7 +8,15 @@ type Props = {
 const Avatar = ({ name, picture }: Props) => {
   return (
     <div className="flex pt1">
-      <img src={picture} className="w-12 h-12 rounded-full mr-4" alt={name} />
+      <Image
+        src={picture}
+        className=""
+        alt={name}
+        width={40}
+        height={40}
+        objectFit="cover"
+        quality={100}
+      />
       <div className="text-xl font-bold">{name}</div>
     </div>
   );
